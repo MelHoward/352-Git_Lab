a0a97b81b58332da6aeacc4666e364a95d70d645
Mel Howard <howardmel98@gmail.com>
2018-08-30 14:26:40 -0500

    Rand

--- GitTest352/Program.cs
+++ GitTest352/Program.cs
@@ -10,7 +10,25 @@ namespace GitTest352
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("I'm thinking of a number between 1 and 100");
+            Random rand = new Random();
+            int randomNumber = rand.Next(0, 100);
+            int input = Console.Read();
+            int i = 0;
+            bool correct = false;
+
+            while(!correct)
+            {
+                if(input == randomNumber) {
+                    correct = true;
+                    break;
+                }
+                else
+                   Console.WriteLine("wrong");
+                   i++;
+                   input = Console.Read();
+            }
+            Console.WriteLine("You guessed ", i, "times before guessing correctly.");
         }
     }
 }
