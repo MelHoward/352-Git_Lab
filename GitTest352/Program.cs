b9e0a62aefd1ca6a6958e3f5160f2f1d2f00d6fa
Mel Howard <howardmel98@gmail.com>
2018-08-30 15:53:21 -0500

    Fixed mindless mistakes

--- GitTest352/Program.cs
+++ GitTest352/Program.cs
@@ -11,24 +11,23 @@ namespace GitTest352
         static void Main(string[] args)
         {
             Console.WriteLine("I'm thinking of a number between 1 and 100");
+
             Random rand = new Random();
             int randomNumber = rand.Next(0, 100);
-            int input = Console.Read();
-            int i = 0;
-            bool correct = false;
 
-            while(!correct)
+            bool correct = false;
+            int guesses = 0;
+
+            int input = Convert.ToInt32(Console.ReadLine());
+            while (!correct)
             {
-                if(input == randomNumber) {
-                    correct = true;
-                    break;
-                }
-                else
-                   Console.WriteLine("wrong");
-                   i++;
-                   input = Console.Read();
+                if (input == randomNumber) { correct = true; guesses += 1; break; }
+                else if (input < randomNumber) { Console.WriteLine("Higher "); guesses += 1; }
+                else if (input > randomNumber) { Console.WriteLine("Lower "); guesses += 1; }
+                input = Convert.ToInt32(Console.ReadLine());
             }
-            Console.WriteLine("You guessed ", i, "times before guessing correctly.");
+            
+            Console.WriteLine("You guessed {0} times before guessing correctly.", guesses);
         }
     }
 }
